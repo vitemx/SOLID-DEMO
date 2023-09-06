d3e6feb96106801399e3deb50e860f3810a1844f
Brandon Vite <bramsel.vite@gmail.com>
2023-09-05 23:22:08 -0600

    Single Responsability

---
+++ 1-SingleResponsability/ExportHelper.cs
@@ -0,0 +1,19 @@
+using System.Text;
+
+namespace SingleResponsability
+{
+    public class ExportHelper
+    {
+        public void ExportStudents(IEnumerable<Student>? students)
+        {
+            string csv = String.Join(",", students?.Select(x => x.ToString()).ToArray());
+            StringBuilder sb = new();
+            sb.AppendLine("Id; Fullname; Grades");
+            foreach (var item in students)
+            {
+                sb.AppendLine($"{item.Id};{item.Fullname};{string.Join("|", item.Grades)}");
+            }
+            System.IO.File.WriteAllText(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Students.csv"), sb.ToString(), Encoding.Unicode);
+        }
+    }
+}
